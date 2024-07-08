1960df163242983e62894869d51db6f4dce32e71
jhauth1 <37121383+jhauth1@users.noreply.github.com>
2024-07-07 20:30:08 -0500

    Set up NNBlackjack to prepare blackjack game for Neural Network

--- NN.py
+++ NN.py
@@ -1,4 +1,15 @@
 import time
 import NNBlackjack as BJ
 import math
+import gym
+import numpy as np
+import PIL.Image
+import tensorflow as tf
+import utils
+
+from pyvirtualdisplay import Display
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.losses import MSE
+from tensorflow.keras.optimizers import Adam
 
